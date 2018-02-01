6b851cb7a7c6c03c3fe9804c3b52c22521332f0c
Lijing Wang <lijing@ualberta.ca>
2018-02-01 20:37:35 +0000

    clean out before new branch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,24 +11,24 @@ $().ready(function(){
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
   const createTweetElement = function(obj){
-      let $tweet = $('<article>').addClass('tweet');
-      let name = obj.user.name
-      let imgUrl = obj.user.avatars.small
-      let handle = obj.handle
-      let content = escape(obj.content.text);
-      let days = Math.floor((Date.now() - obj['created_at']) / 86400000);
-      let newTweet = [
-        `<header><img src=${imgUrl}></img><h2>${name}</h2><span>${handle}</span></header>`,
-        `<p>${content}</p><footer><p>${days} days ago</p>`,
-        '<i class="fa fa-flag" aria-hidden="true"></i>',
-        '<i class="fa fa-retweet" aria-hidden="true"></i>',
-        '<i class="fa fa-heart" aria-hidden="true"></i></footer>'
-        ];
-      $tweet.append(newTweet.join(''));
-      return $tweet;
+    let $tweet = $('<article>').addClass('tweet');
+    let name = obj.user.name;
+    let imgUrl = obj.user.avatars.small;
+    let handle = obj.user.handle;
+    let content = escape(obj.content.text);
+    let days = Math.floor((Date.now() - obj['created_at']) / 86400000);
+    let newTweet = [
+      `<header><img src=${imgUrl}></img><h2>${name}</h2><span>${handle}</span></header>`,
+      `<p>${content}</p><footer><p>${days} days ago</p>`,
+      '<i class="fa fa-flag" aria-hidden="true"></i>',
+      '<i class="fa fa-retweet" aria-hidden="true"></i>',
+      '<i class="fa fa-heart" aria-hidden="true"></i></footer>'
+    ];
+    $tweet.append(newTweet.join(''));
+    return $tweet;
   };
 
   const renderTweets = function(array){
@@ -43,53 +43,49 @@ $().ready(function(){
       method: 'GET'
     }).done(function(data){
       renderTweets(data);
-    })
+    });
   });
 
-//submission listener and handler with flash message
-  $('.new-tweet form input').on('click', function(event) {
+  $(function hideFlashMessage (){
+    $('#flash-message').hide();
+  });
+
+  //submission listener and handler with flash message
+  $('#submitBtn').on('click', function(event) {
     event.preventDefault();
-//cannot send empty tweet
-    if (!$(this).prev('textarea').val()) {
-      const errorMessage = [
-        '<div class="flash-message">',
-        '<p>Unfortunately! Cannot send empty tweet...</p>',
-        '<button><i class="fa fa-times" aria-hidden="true"></i></button></div>'
-      ];
-      $('.container').prepend(errorMessage.join('')).hide().fadeIn(500);
-    } else if ($(this).prev('textarea').val().length > 140) {
-//cannot send tweet more than 140 characters
-      const errorMessage = [
-        '<div class="flash-message">',
-        '<p>Unfortunately! Cannot send tweet more than 140 characters...</p>',
-        '<button><i class="fa fa-times" aria-hidden="true"></i></button></div>'
-      ];
-      $('.container').prepend(errorMessage.join(''));
+    //cannot send empty tweet
+    if (!$('#new-tweet-content').val()) {
+      const errorMessage = 'Unfortunately! Cannot send empty tweet...';
+      $('#flash-message').children('p').html(errorMessage);
+      $('#flash-message').slideDown('slow');
+    } else if ($('#new-tweet-content').val().length > 140) {
+      //cannot send tweet more than 140 characters
+      const errorMessage = 'Unfortunately! Cannot send tweet more than 140 characters...';
+      $('#flash-message').children('p').html(errorMessage);
+      $('#flash-message').slideDown('slow');
     } else {
-      $('.flash-message').remove();
-      const queryString = $(this).prev('textarea').serialize();
+      $('#flash-message').hide('slow');
+      const queryString = $('#new-tweet-content').serialize();
       $.ajax({
         url: '/tweets/',
         method: 'POST',
         data: queryString
-        }).done(function(newtweet){
-          $newtweet = createTweetElement(newtweet);
-          $('#tweet-container').prepend($newtweet);
-        });
-    };
+      }).done(function(newtweet){
+        $newtweet = createTweetElement(newtweet);
+        $('#tweet-container').prepend($newtweet);
+      });
+    }
   });
 
-//close flash message listener
-  $('main').on('click', '.fa-times', function(){
-    $($(this).parent()).fadeOut(500, function(){
-      $($(this).parent()).remove();
-    });
+  //close flash message listener
+  $('.fa-times').on('click', function(){
+    $('#flash-message').slideUp('slow');
   });
 
-//compose handler
+  //compose handler
   $('.composeBtn').on('click', function(){
     $('.new-tweet').slideToggle(function(){
-      $('.new-tweet').children('form').children('textarea').focus();
+      $('#new-tweet-content').focus();
     });
   });
 
